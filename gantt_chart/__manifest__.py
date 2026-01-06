ff29ec62d7798ba62d52e3e1725ea537863ed5cc
zekrom-tech <zekromtechllc@gmail.com>
2026-01-06 09:55:46 +0530

    ba

--- gantt_chart/__manifest__.py
+++ gantt_chart/__manifest__.py
@@ -190,7 +190,7 @@ For support, customization requests, or feedback:
 
     # Module Images (for Odoo App Store)
     'images': [
-        'static/description/banner.JPG',
+        'static/description/banner.jpg',
         'static/description/icon.png',
         'static/description/screenshot_1_overview.png',
         'static/description/screenshot_2_colorpicker.png',
